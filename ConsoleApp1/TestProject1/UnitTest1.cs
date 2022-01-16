2f99562cf9d95710569f7f9aa5f5b8680dc33165
anandubale <au26199@gmail.com>
2022-01-16 11:54:20 +0530

    Validate User Entry FirstName, lastname Email and more

---
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrange
+        //Act
+        //Assert
+        [TestMethod]
+        public void GivenFirstName_WhenCheck_ShouldReturnTrue()
+        {
+            RegexPatterns name = new RegexPatterns();
+            bool result = name.ValidateName("Anand", RegexPatterns.FIRSTNAME_REGEX);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenLastName_WhenCheck_ShouldReturnTrue()
+        {
+            RegexPatterns lastname = new RegexPatterns();
+            bool resultlastname = lastname.ValidateLastName("Ubale", RegexPatterns.LASTNAME_REGEX);
+            Assert.IsTrue(resultlastname);
+        }
+        [TestMethod]
+        public void GivenPhoneNum_WhenCheck_ShouldReturnTrue()
+        {
+            RegexPatterns phonenum = new RegexPatterns();
+            bool resultphonenum = phonenum.ValidatePhoneNum("91 1234567890", RegexPatterns.PHONENUM_REGEX);
+            Assert.IsTrue(resultphonenum);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenCheck_ShouldReturnTrue()
+        {
+            RegexPatterns emailid = new RegexPatterns();
+            bool resultemailid = emailid.ValidateEmail("AnandUbale11gmail.com", RegexPatterns.EMAIL_REGEX);
+            Assert.IsTrue(resultemailid);
+        }
+        [TestMethod]
+        public void TGivenPassword_WhenCheck_ShouldReturnTrue()
+        {
+            RegexPatterns pass = new RegexPatterns();
+            bool resultpass = pass.ValidatePassword("123A@dsd659", RegexPatterns.PASSWORD_REGEX);
+            Assert.IsTrue(resultpass);
+        }
+    }
+}
