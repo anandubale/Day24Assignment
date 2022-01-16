2a04d12d21703840c8ddba726db94ea5c67f37cc
anandubale <au26199@gmail.com>
2022-01-16 12:26:04 +0530

    Validaton of User using Lambda Expression

--- ConsoleApp1/ConsoleApp1/ReflectionOnUserRegistration.cs
+++ ConsoleApp1/ConsoleApp1/ReflectionOnUserRegistration.cs
@@ -9,24 +9,23 @@ namespace ConsoleApp1
     {
         public static void ReflectionTest()
         {
-
             Type type = Type.GetType("RegexTest.RegexProgram");
             Console.WriteLine("Full Name :{0}", type.FullName);
             Console.WriteLine("Class name is : {0}", type.Name);
-            Console.WriteLine("--------------------------------\nMethods in RegexProgram class\n--------------------------------");
+            Console.WriteLine("\nMethods in RegexProgram class");
             MethodInfo[] methods = type.GetMethods();
             foreach (MethodInfo method in methods)
             {
                 Console.WriteLine(method.ReturnType.Name + " " + method.Name);
             }
-            Console.WriteLine("--------------------------------\nProperties in RegexProgram class\n--------------------------------");
+            Console.WriteLine("\nProperties in RegexProgram class");
             PropertyInfo[] properties = type.GetProperties();
             foreach (PropertyInfo property in properties)
             {
                 Console.WriteLine(property.PropertyType.Name + " " + property.Name);
             }
 
-            Console.WriteLine("--------------------------------\nConstructors in RegexProgram class\n--------------------------------");
+            Console.WriteLine("\nConstructors in RegexProgram class");
             ConstructorInfo[] constructors = type.GetConstructors();
             foreach (ConstructorInfo constructor in constructors)
             {
--- ConsoleApp1/TestProject1/UnitTest1.cs
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -5,6 +5,7 @@ using System;
 
 namespace TestProject1
 {
+
     [TestClass]
     public class UnitTest1
     {   //Arrange
@@ -13,36 +14,36 @@ namespace TestProject1
         [TestMethod]
         public void GivenFirstName_WhenCheck_ShouldReturnTrue()
         {
-            RegexPatterns firstname = new RegexPatterns();
-            bool result = firstname.ValidateName("Anand");
+            RegexPatterns name = new RegexPatterns();
+            bool result = name.ValidateName("Anand", RegexPatterns.FIRSTNAME_REGEX);
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void GivenLastName_WhenCheck_ShouldReturnTrue()
         {
             RegexPatterns lastname = new RegexPatterns();
-            bool resultlastname = lastname.ValidateLastName("Ubale");
+            bool resultlastname = lastname.ValidateLastName("Ubale", RegexPatterns.LASTNAME_REGEX);
             Assert.IsTrue(resultlastname);
         }
         [TestMethod]
         public void GivenPhoneNum_WhenCheck_ShouldReturnTrue()
         {
             RegexPatterns phonenum = new RegexPatterns();
-            bool resultphonenum = phonenum.ValidatePhoneNum("91 1234567890");
+            bool resultphonenum = phonenum.ValidatePhoneNum("91 1234567890", RegexPatterns.PHONENUM_REGEX);
             Assert.IsTrue(resultphonenum);
         }
         [TestMethod]
         public void GivenEmailId_WhenCheck_ShouldReturnTrue()
         {
             RegexPatterns emailid = new RegexPatterns();
-            bool resultemailid = emailid.ValidateEmail("ANANDUBALE11@gmail.com");
+            bool resultemailid = emailid.ValidateEmail("Anandubale11@gmail.com", RegexPatterns.EMAIL_REGEX);
             Assert.IsTrue(resultemailid);
         }
         [TestMethod]
         public void TGivenPassword_WhenCheck_ShouldReturnTrue()
         {
             RegexPatterns pass = new RegexPatterns();
-            bool resultpass = pass.ValidatePassword("46a4s5@Aknac");
+            bool resultpass = pass.ValidatePassword("123A@dsd659", RegexPatterns.PASSWORD_REGEX);
             Assert.IsTrue(resultpass);
         }
     }
